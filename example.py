7e5733ad457ba9b6f17b092040cacf6494fc1de1
Andre Tan <tanandre93@gmail.com>
2016-07-16 22:37:09 +0800

    update example.py with proper flow

--- example.py
+++ example.py
@@ -1,10 +1,26 @@
 import pandas as pd
 import ezcluster
 
-iris = pd.read_csv('iris.csv') # change to github link
+# load the iris dataset
+iris = pd.read_csv('https://raw.githubusercontent.com/thisisandreeeee/ezcluster/master/iris.csv')
 species = iris['species']
 iris.drop('species', axis = 1, inplace = True)
 
+# initialize the kmeans class
 ezc = ezcluster.Kmeans(iris)
-ezc.fit()
+
+# find the optimal number of clusters
+num_of_clusters = ezc.optimal_k(min_k=1, max_k=10, num_iters=100)
+
+# plot the gap statistic plots
+ezc.plot()
+
+# return a model with optimal number of clusters
+model = ezc.fit(n_clusters = num_of_clusters)
+print(num_of_clusters)
+
+# save instance
+ezc.save_model()
+
+# save labeled dataset to csv
 ezc.write_csv()
